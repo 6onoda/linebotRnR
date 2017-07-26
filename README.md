3e8ccc0f14c2055cb7cfaa6928f3e2114d7a61a9
Yasuhiro Onoda <6onoda@gmail.com>
2017-07-26 12:04:36 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# linebottalk
-Demo server program for line bot and talk api
+# linebotRnR
+Demo server program for line bot to ask IBM Watson Retrieve And Rank
+
+-Please modify config.json for your envinronmant before use this.
+-This program assumes your RnR has 'title' and 'url' values.
+ and retuens to LINE.
+ If not, please also modify them.  
--- server/server.py
+++ server/server.py
@@ -5,6 +5,10 @@ import os
 import json
 import urllib.request, urllib.parse
 
+import pysolr
+from watson_developer_cloud import RetrieveAndRankV1
+
+
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -15,7 +19,6 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
-
 # Emit Bluemix deployment event
 cf_deployment_tracker.track()
 
@@ -23,12 +26,25 @@ app = Flask(__name__)
 
 config_file = open('config.json' , 'r')
 config = json.load(config_file)
-talk_api_key = config["talkApiKey"]
+# talk_api_key = config["talkApiKey"]
+
 line_channel_token = config["lineChannelToken"]
 line_channel_secret = config["lineChannelSecret"]
 
+rnr_user = config["RnR_USERNAME"]
+rnr_pass = config["RnR_PASSWORD"]
+rnr_collection = config["RnR_COLLECTION"]
+rnr_cluster_id = config["RnR_CLUSTER_ID"]
+rnr_ranker_id  = config["RnR_RANKER_ID"]
+
+retrieve_and_rank = RetrieveAndRankV1(
+      username = rnr_user,
+      password = rnr_pass)
+
+solrclient = retrieve_and_rank.get_pysolr_client(rnr_cluster_id , rnr_collection)
+
 # On Bluemix, get the port number from the environment variable PORT
-# When running this app on the local machine, default the port to 8080
+# When running this app on the local machine, default the port to 8081
 port = int(os.getenv('PORT', 8080))
 
 
@@ -56,23 +72,30 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    data = {
-        "apikey": talk_api_key,
-        "query": event.message.text
-    }
+ 
+    #query_string = event.message.text.encode("utf-8")
+    query_string = urllib.parse.quote(event.message.text.encode("utf-8"), safe='')
+    results = solrclient._send_request("GET", path="/fcselect?q=%s&ranker_id=%s&wt=json&fl=ranker.confidence,title,url" % (query_string, rnr_ranker_id))
 
-    data = urllib.parse.urlencode(data).encode("utf-8")
-    with urllib.request.urlopen("https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk", data=data) as res:
-        #response = res.read().decode("utf-8")
-        reply_json = json.loads(res.read().decode("unicode_escape"))
+    reply=""
+    
+    if len(json.loads(results)["response"]["docs"]) == 0:
+       reply="見つかりませんでした。"
 
-        if reply_json['status'] == 0:
-            reply = reply_json['results'][0]['reply']
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text=reply))
-   
-
+    else:
+    #    for doc in json.loads(results)["response"]["docs"]:
+        for i, doc in enumerate(json.loads(results)["response"]["docs"]):
+            reply += doc["title"] + "\n" + doc["url"] + "\n\n"
+            # max 3 docs
+            if i >= 2:
+                break       
+            # only one for confidence == 0 if >= 3 items 
+            if doc["ranker.confidence"] <= 0 :
+                break
+                
+    line_bot_api.reply_message(
+          event.reply_token,
+          TextSendMessage(text=reply.rstrip() ))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port, debug=True)
